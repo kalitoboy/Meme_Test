247c0f130b1be3ff005c0c6ad3c53d8d697203f2
kai zhang <kai.zhang@uni-ulm.de>
2020-01-17 13:41:58 +0100

    timer implemented in Neck, working with push of a button

--- app/src/main/java/com/mhcibasics/memetest/Fragments/Training/Neck.java
+++ app/src/main/java/com/mhcibasics/memetest/Fragments/Training/Neck.java
@@ -3,14 +3,44 @@ package com.mhcibasics.memetest.Fragments.Training;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.mhcibasics.memetest.R;
 
 public class Neck extends AppCompatActivity {
 
+    TextView timer;
+
+    Button start;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_neck);
+
+        timer = findViewById(R.id.timer);
+        start = findViewById(R.id.training_start);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (v.getId() == start.getId()) {
+                    CountDownTimer countDownTimer = new CountDownTimer(30000, 1000) {
+                        @Override
+                        public void onTick(long millisUntilFinished) {
+                            timer.setText("seconds remaining: " + millisUntilFinished / 1000);
+                        }
+
+                        @Override
+                        public void onFinish() {
+                            timer.setText("Done!");
+                        }
+                    }.start();
+                }
+            }
+        });
     }
 }
